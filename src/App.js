9cdb0a25c96f1963c4583cc6dfd79d457ca5cbcb
Sindhoora Ponnuswamy <sindhoorap.30@gmail.com>
2024-04-03 15:28:54 +0100

    fetched data from json file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [testDetails, setTestDetails] = useState([]);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then(response => response.json())
+      .then(data => {
+        setTestDetails(data.testDetails);
+        console.log(data.testDetails); // Check if data is fetched correctly
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  
+
+  const updateData = newData => {
+    setTestDetails(newData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test Details</h1>
+      <button onClick={() => updateData([{ TestId: 4, Description: "New Description", Test: "New Test", PeakNumber: 5, PeakDuration: "New Duration", ValidTestRun: true, Status: "New Status" }])}>
+        Update Data
+      </button>
+      <table>
+  <tbody>
+    {testDetails.length > 0 && Object.keys(testDetails[0]).map(key => (
+      <tr key={key}>
+        <td>{key}</td>
+      
+        <td>{testDetails[0][key]}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
+
     </div>
   );
 }
